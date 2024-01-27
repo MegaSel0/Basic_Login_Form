e43e01893cf6f2a15ba9f58e4bcd60ce0efd6519
MegaSel0 <samsa4431@gmail.com>
2024-01-28 00:31:43 +0330

    Changes_1

--- loginmanager.cpp
+++ loginmanager.cpp
@@ -6,7 +6,7 @@ LoginManager::LoginManager(QObject *parent)
 
 void LoginManager::managerFields(const QString &username, const QString &password)
 {
-    if(username == "Afshin" && password == "1234"){
+    if(username == "zz6" && password == "1234"){
         emit loginSignalChanged("Login :)");
     }else{
         emit loginSignalChanged("Sorry username or password invalid");
